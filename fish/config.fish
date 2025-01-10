35474ad317326f25a9ff27aa9123623a54f438cd
ikalchev <ivan@latticeflow.ai>
2025-01-10 08:55:37 +0200

    fish config to read env specific functions

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     atuin init fish | source
     /opt/homebrew/bin/brew shellenv | source
+    # Extend the locations where fish looks for functions to
+    # include "machine-specific" functions. For example, these
+    # are functions not common between machines.
+    set -a fish_function_path ~/.config/more_fish 
 end
