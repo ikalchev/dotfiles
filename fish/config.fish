9bbb78e80c4102e4e23f090b8cf57cfdfff1c2f3
Ivan Kalchev <ivan.kalchev@icloud.com>
2025-01-11 11:59:18 +0200

    Extend fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,10 @@ if status is-interactive
     # include "machine-specific" functions. For example, these
     # are functions not common between machines.
     set -a fish_function_path ~/.config/more_fish
-    
+
+    # Machine-specific config.
+    source ~/.config/more_fish/config.fish
+
     # Config the hydro prompt.
     set --global hydro_color_pwd green
     set --global hydro_color_git cyan
